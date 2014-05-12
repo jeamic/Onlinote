ecfe2c61191ad7cbb43d1c69e63c5e19fc059428
girque <qu.girard@gmail.com>
2014-05-12 14:43:19 +0200

    ajout d'un nom de salle dans la bdd, mise à jour du modele

--- TestGitIgnore/src/modele/basedao/Salle.java
+++ TestGitIgnore/src/modele/basedao/Salle.java
@@ -2,8 +2,15 @@ package modele.basedao;
 
 public class Salle implements java.io.Serializable {
 	private int idSalle;
+	private String nomSalle;
 	
-	public Salle () {
+	public String getNomSalle() {
+        return nomSalle;
+    }
+    public void setNomSalle(String nomSalle) {
+        this.nomSalle = nomSalle;
+    }
+    public Salle () {
 	}
 	public Salle (int idSalle) {
 		this.idSalle = idSalle;
