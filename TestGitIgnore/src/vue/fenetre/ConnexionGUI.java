7acdd305dd68a3410671e8918b65eab3c6a043ea
Mika <mickaeljean83@gmail.com>
2014-05-12 13:59:16 +0200

    Fichier listFonction

--- TestGitIgnore/src/vue/fenetre/ConnexionGUI.java
+++ TestGitIgnore/src/vue/fenetre/ConnexionGUI.java
@@ -30,27 +30,28 @@ public class ConnexionGUI {
 	public ConnexionGUI() {
 		
 		frmConnexion = new JFrame();
+		frmConnexion.setResizable(false);
 		
 		frmConnexion.setTitle("Connexion");
-		frmConnexion.setBounds(100, 100, 448, 212);
+		frmConnexion.setBounds(100, 100, 500, 200);
 		frmConnexion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmConnexion.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(614, 295, 223, 20);
+		textField.setBounds(158, 42, 223, 20);
 		frmConnexion.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNomDutilisateur = new JLabel("Email :");
-		lblNomDutilisateur.setBounds(491, 298, 129, 14);
+		lblNomDutilisateur.setBounds(35, 45, 129, 14);
 		frmConnexion.getContentPane().add(lblNomDutilisateur);
 		
 		JLabel lblMotDePasse = new JLabel("Mot de passe :");
-		lblMotDePasse.setBounds(491, 331, 129, 14);
+		lblMotDePasse.setBounds(35, 78, 129, 14);
 		frmConnexion.getContentPane().add(lblMotDePasse);
 		
 		JButton btnConnexion = new JButton("Connexion");
-		btnConnexion.setBounds(665, 365, 129, 23);
+		btnConnexion.setBounds(209, 112, 129, 23);
 		frmConnexion.getContentPane().add(btnConnexion);
 
 		/* écouteur d'évènements sur le bouton connexion */
@@ -70,9 +71,8 @@ public class ConnexionGUI {
 		/* activer la connexion quand on presse entrée */
 		frmConnexion.getRootPane().setDefaultButton(btnConnexion);
 		passwordField = new JPasswordField();
-		passwordField.setBounds(614, 328, 223, 20);
+		passwordField.setBounds(158, 75, 223, 20);
 		frmConnexion.getContentPane().add(passwordField);
-		frmConnexion.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frmConnexion.setVisible(true);
 	}
 
